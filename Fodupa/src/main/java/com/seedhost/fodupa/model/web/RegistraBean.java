c35e9f99f3e699c132e61f3054766759787b8378
Ferch2109 <marifer.gusanito@gmail.com>
2018-04-09 14:56:26 -0500

    Bean para registrar

---
+++ Fodupa/src/main/java/com/seedhost/fodupa/model/web/RegistraBean.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.seedhost.fodupa.web;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+/**
+ *
+ * @author fergch97
+ */
+@ManagedBean
+@SessionScoped
+public class RegistraBean{
+    private String nombre;
+    private String apPaterno;
+    private String apMaterno;
+    private String correo;
+    private String contrasena;
+    private String confirm;
+    private byte[] foto;
+    private Carrera carrera;
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getApPaterno(){
+        return apPaterno;
+    }
+    public String getApMaterno(){
+        return apMaterno;
+    }
+    public String getCorreo(){
+        return correo;
+    }
+    public String getContrasena(){
+        return contraseña;
+    }
+    public String getConfirm(){
+        return confirm;
+    }
+    public byte[] getFoto(){
+        return foto;
+    }
+    public Carrera getCarrera(){
+        return carrera;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setApPaterno(String apPaterno){
+        this.apPaterno = apPaterno;
+    }
+    public void setApMaterno(String apMaterno){
+        this.apMaterno = apMaterno;
+    }
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+    public void setContrasena(String contraseña){
+        this.contraseña = contraseña;
+    }
+    public void setConfirm(String confirm){
+        this.confirm = confirm;
+    }
+    public void setFoto(byte[] foto){
+        this.foto = foto;
+    }
+    public void setCarrera(Carrera carrera){
+        this.carrera = carrera;
+    }
+    
+}
