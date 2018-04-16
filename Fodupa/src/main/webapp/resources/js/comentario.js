abe9472dc48a260d8bece57e61458b73e11e41ed
Rodrigo Sánchez <rodrigosanchez@ciencias.unam.mx>
2018-04-15 21:37:43 -0500

    Comportamiento de .js agregado
    Archivo comentario.js aplicado en la interfaz gráfica

--- Fodupa/src/main/webapp/resources/js/comentario.js
+++ Fodupa/src/main/webapp/resources/js/comentario.js
@@ -1,9 +1,51 @@
 /**
- * [validateTitleLength description]
- * @return {[type]} [description]
+ * Gets a copy of the string with all duplicate characters removed.
+ * @param {string} str Input string which will be duplicated and whose duplicate
+ *                     characters will be removed.
+ * @returns {string}   A copy of str with all duplicate characters removed.
+ */
+function uniqChars(str) {
+    return str.replace(/[\s\S](?=([\s\S]+))/g, function(c, s) {
+        return s.indexOf(c) + 1 ? '' : c;
+    });
+}
+
+/**
+ * [validateContentLength contenido]
+ * @return {[type]} [contenido]
  */
 function validateContentLength(){
-    return $("#form_content\\:contenido").val().replace(/^\s*/, "").length > 0;
+    return $("#form_comment\\:contenido").val().replace(/^\s*/, "").length > 0;
+}
+
+/**
+ * [validateDetails description]
+ * @return {[type]} [description]
+ */
+function validateContent(){
+    var pattern = /^[A-Za-z0-9\s¿?+-_.*/{}()%&amp;#$@|!¡;,:áé\níóúÁÉÍÓÚñÑ"]*$/;
+    return pattern.test($("#form_comment\\:contenido").val());
+};
+
+/**
+ * [getContenidoInvalidChars contenido]
+ * @return {[type]} [contenido]
+ */
+function getContentInvalidChars(){
+    var pattern = /[A-Za-z0-9\s¿?+-_.*/{}()%&amp;#$@|!¡;,:áé\níóúÁÉÍÓÚñÑ"]/g;
+    return uniqChars($("#form_comment\\:contenido").val().replace(pattern,""));
+};
+
+/**
+ * [successStatusContent contenido]
+ * @return {[type]} [contenido]
+ */
+function successStatusContent(){
+    $("#form_comment\\:contenido").addClass('is-valid');
+    $("#form_contenido\\:contenido").removeClass('is-invalid');
+    $("label[for='contenido']").addClass('text-success');
+    $("label[for='contenido']").removeClass('text-danger');
+    $("#error_contenido").addClass("hide");
 }
 
 /**
@@ -11,21 +53,32 @@ function validateContentLength(){
  * @return {[type]} [contenido]
  */
 function errorStatusContent(){
-    $("#form_content\\:contenido").addClass('is-invalid');
-    $("#form_content\\:contenido").removeClass('is-valid');
+    $("#form_comment\\:contenido").addClass('is-invalid');
+    $("#form_comment\\:contenido").removeClass('is-valid');
     $("label[for='contenido']").addClass('text-danger');
     $("label[for='contenido']").removeClass('text-success');
     $("#error_contenido").removeClass("hide");
 }
 
 window.onload = function(){
-
-    //Input [conten] event handler
-    $("#form_content\\contenido").on("keyup", function(){
+    
+    //Input [contenido] event handler
+    $("#form_comment\\:contenido").on("keyup", function(){
         if(!validateContentLength()){
             errorStatusContent();
-            $("#error_content").text("Lo sentimos, la longitud del título debe contener a lo menos 1 caracter.");
+            $("#error_contenido").text("Lo sentimos, la longitud del título debe contener al menos 1 caracter.");
+        } else if(!validateContent()){
+            errorStatusContent();
+            $("#error_contenido").text("Lo sentimos, los siguientes caracteres no son válidos: " + getContentInvalidChars());
         }
+        else
+            successStatusContent();
+    });
+
+    //Submit form event handler
+    $("#form_comment").submit(function(e){
+        if(!validateContentLength() || !validateContent())
+            return false;
     });
 
 }
