fa8fa9cfa2971a21e38eab61d3a70920ba0183ab
Ferch2109 <marifer.gusanito@gmail.com>
2018-04-23 01:58:40 -0500

    Se agrega la dependencia para javax.mail

--- Fodupa/src/main/java/com/seedhost/fodupa/controller/RegistraController.java
+++ Fodupa/src/main/java/com/seedhost/fodupa/controller/RegistraController.java
@@ -17,12 +17,10 @@ import java.io.IOException;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
-import java.util.Date;
+import java.util.Properties;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -30,6 +28,9 @@ import javax.faces.context.FacesContext;
 import javax.persistence.EntityManagerFactory;
 
 import static javax.faces.context.FacesContext.getCurrentInstance;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 
 /**
@@ -130,11 +131,49 @@ public class RegistraController implements Serializable {
         usuario.setCarreraList(carreras);
         usuario.setFoto(foto);
         
+        //CORREO
+        String destinatario = registra_bean.getCorreo();
+        String asunto = "Confirmación de registro";
+        String link = ""; //Pendiente...
+        String cuerpo = "Haz click en el siguiente enlace para confirmar tu registro:\n"+link;
+        enviar(destinatario,asunto,cuerpo);
+        
         u_jpaController = new UsuarioJpaController(emf);
         u_jpaController.create(usuario);
         
     }
     
+    private int enviar(String destinatario, String asunto, String cuerpo) {
+        // Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el remitente también.
+        String remitente = "fodupa@gmail.com";  //Para la dirección nomcuenta@gmail.com
+        String password = "seedhost123";
+
+        Properties props = System.getProperties();
+        props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
+        props.put("mail.smtp.user", "fodupa");
+        props.put("mail.smtp.clave", password);    //La clave de la cuenta
+        props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
+        props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
+        props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(remitente));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));   //Se podrían añadir varios de la misma manera
+            message.setSubject(asunto);
+            message.setText(cuerpo);
+            Transport transport = session.getTransport("smtp");
+            transport.connect("smtp.gmail.com", remitente, password);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (MessagingException me) {return -1;}
+        
+        return 1;
+    }
+
     public List<Carrera> getCarreras() {
         return carreras;
     }
